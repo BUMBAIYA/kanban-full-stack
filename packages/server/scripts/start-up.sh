8708e3eef2a409fce150791d29ee8f6659434680
Amit Chauhan <amitchauhan263871@gmail.com>
2023-08-14 02:20:33 +0530

    fixed database migration on server startup

--- packages/server/scripts/start-up.sh
+++ packages/server/scripts/start-up.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# TODO
-# Used for docker if database is not ready for migration
+# check if database is ready for migration than start the server
+./scripts/wait-for-it.sh -h postgres -p 5432 -t 5
+
+npm run db:migrate
+npm run db:migrate-deploy
+npm run db:generate
+npm run start
