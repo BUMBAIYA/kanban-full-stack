db2195adb654aaba3db7bf5a7d2814bcb96c0758
Amit Chauhan <amitchauhan263871@gmail.com>
2023-08-15 14:03:36 +0530

    fixed ports of docker compose

--- packages/server/scripts/start-up.sh
+++ packages/server/scripts/start-up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # check if database is ready for migration than start the server
-./scripts/wait-for-it.sh -h postgres -p 5432 -t 5
+./scripts/wait-for-it.sh -h postgres -p 5432 -t 10
 
 npm run db:migrate
 npm run db:migrate-deploy
